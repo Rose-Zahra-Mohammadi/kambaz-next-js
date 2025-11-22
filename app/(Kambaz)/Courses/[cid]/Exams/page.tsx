fd7e752ddf8bec352aaa73f1034e986c579855de
Rosa Mohammadi <rosamohammadi@mac.mynetworksettings.com>
2025-11-22 16:49:58 -0500

    added the links for Exams and Project

---
+++ app/(Kambaz)/Courses/[cid]/Exams/page.tsx
@@ -0,0 +1,7 @@
+export default function Exams() {
+  return (
+    <div>
+      <h2>Exams</h2>
+    </div>
+  );
+}
---
+++ app/(Kambaz)/Courses/[cid]/Project/page.tsx
@@ -0,0 +1,7 @@
+export default function Project() {
+  return (
+    <div>
+      <h2>Project</h2>
+    </div>
+  );
+}
